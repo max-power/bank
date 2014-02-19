9f3d0dbbb04af2d3c830edaad780582bd2acca8f
Kevin <kevin.melchert@gmail.com>
2014-02-19 16:38:57 +0100

    fix bic validator

--- lib/bank/validators/bic_validator.rb
+++ lib/bank/validators/bic_validator.rb
@@ -2,6 +2,6 @@ require 'active_model/validator'
 
 class BicValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, :invalid) unless BIC.valid?(value)
+    record.errors.add(attribute, :invalid) unless Bank::BIC.valid?(value)
   end
 end
--- lib/bank/version.rb
+++ lib/bank/version.rb
@@ -1,3 +1,3 @@
 module Bank
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
