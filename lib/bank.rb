a7900356990a34a0aad69e7437ecc9f49ca0fd5e
Jan Deelstra <jan@deelstra.org>
2014-06-12 11:09:34 +0200

    And added double quotes here.

--- lib/bank.rb
+++ lib/bank.rb
@@ -4,6 +4,6 @@ require 'yaml'
 
 module Bank
   def self.load_specifications(name)
-    YAML.load_file Pathname.new(__FILE__).dirname.parent + 'data' + '#{name}.yml'
+    YAML.load_file Pathname.new(__FILE__).dirname.parent + 'data' + "#{name}.yml"
   end
 end
